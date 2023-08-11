e94f4448467a80f16aa31b39c1ea14051a04cc3f
hall488 <hall488@purdue.edu>
2023-08-11 15:05:22 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js',
+        // Runtime code for hot module replacement
+        hot: 'webpack/hot/dev-server.js',
+        // Dev server client for web socket transport, hot and live reload logic
+        client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        // Dev server client for web socket transport, hot and live reload logic
+        hot: false,
+        client: false,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+        // Plugin for hot module replacement
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+
+};
