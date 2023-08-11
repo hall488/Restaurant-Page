08108405942aa5cc53e4bf959acdc4c203d049bb
hall488 <hall488@purdue.edu>
2023-08-11 15:05:22 -0400

    Move hot and client entry from common to dev

--- webpack.common.js
+++ webpack.common.js
@@ -6,9 +6,6 @@ module.exports = {
     entry: {
         app: './src/index.js',
         // Runtime code for hot module replacement
-        hot: 'webpack/hot/dev-server.js',
-        // Dev server client for web socket transport, hot and live reload logic
-        client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
     },
     
     module: {
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,11 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    entry : {
+        hot: 'webpack/hot/dev-server.js',
+        // Dev server client for web socket transport, hot and live reload logic
+        client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
+    },
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
